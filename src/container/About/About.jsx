a331092c251eb47ee519aafc4aae528834a30053
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-07-29 15:36:01 +0200

    Skills icon's background is changed

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import {urlFor, client} from '../../client';
-import { AppWrap } from "../../wrapper";
+import { AppWrap, MotionWrap } from "../../wrapper";
 import './About.scss';
 
 
@@ -44,4 +44,8 @@ const About = () => {
     )
 }
 
-export default AppWrap(About, 'about');
+export default AppWrap(
+    MotionWrap(About, 'app__about'),
+    'about',
+    "app__whitebg"
+);
--- src/container/Works/Works.jsx
+++ src/container/Works/Works.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { AiFillEye, AiFillGithub } from "react-icons/ai";
 import { motion } from "framer-motion";
 
-import { AppWrap } from "../../wrapper";
+import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 import "./Works.scss";
 
@@ -116,4 +116,9 @@ const Works = () => {
   );
 };
 
-export default AppWrap(Works, "work");
+export default AppWrap(
+  MotionWrap(Works, 'app__work' ),
+  "work",
+  "app__primarybg"
+);
+
